f348d686a36791d27022437b17fcb6bd9319a23e
Steven4565 <stevenchristian4565@gmail.com>
2024-02-18 21:33:59 +0700

    added users page

---
+++ src/routes/user/[userId]/+page.server.ts
@@ -0,0 +1,26 @@
+import prisma from '$lib/server/prisma';
+
+export async function load(event) {
+	if (!event.locals.user) throw new Error('Unauthorized');
+
+	const firstClass = await prisma.userClass.findFirst({ where: { userId: event.locals.user.id } });
+	if (!firstClass) throw new Error('User has not joined any classes');
+
+	const userActivities = prisma.userActivities.findMany({
+		where: {
+			userId: event.locals.user.id,
+			classId: firstClass.classId
+		},
+		include: {
+			actionType: true
+		}
+	});
+
+	const exp = (await userActivities).reduce((exp, { actionType }) => {
+		return exp + actionType.experience;
+	}, 0);
+
+	return {
+		user: { ...event.locals.user, exp }
+	};
+}
---
+++ src/routes/user/[userId]/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let data;
+	$: ({ user } = data);
+	import { Heading } from 'flowbite-svelte';
+</script>
+
+<section>
+	<div class="flex">
+		<div class="h-max">
+			<img src="https://placekitten.com/200/200" alt="" />
+		</div>
+		<div>
+			<Heading>{user.username}</Heading>
+			<Heading>{user.exp} Points</Heading>
+			<div>
+				<!-- TODO: input tags here -->
+			</div>
+		</div>
+	</div>
+</section>

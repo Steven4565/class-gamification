10befa077f32b79d8c85b84e2e8d7409cb14f786
steven <stevenchristian4565@gmail.com>
2024-03-01 12:26:17 +0700

    changed navbar to use skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
-	import NavbarLayout from '$lib/components/NavbarLayout.svelte';
-	import { page } from '$app/stores';
-
 	import '../app.pcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import NavbarLayout from '$lib/components/NavbarLayout.svelte';
+	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
+
+	import { page } from '$app/stores';
 
 	export let data;
 </script>
 
-{#if !$page.url.pathname.startsWith('/login') && !$page.url.pathname.startsWith('/admin')}
-	<NavbarLayout name={data.user?.username ?? ''} id={data.user?.id ?? ''} />
-{/if}
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">
+		{#if !$page.url.pathname.startsWith('/login') && !$page.url.pathname.startsWith('/admin')}
+			<NavbarLayout name={data.user?.username ?? ''} id={data.user?.id ?? ''} />
+		{/if}
+	</svelte:fragment>
+	<slot />
+</AppShell>

55e810a1822cc94474d58a05a9fa8a42c95ff8ed
steven <stevenchristian4565@gmail.com>
2024-03-17 01:05:54 +0700

    added flex wrap to home page

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -4,6 +4,7 @@ import { Argon2id } from 'oslo/password';
 
 import type { Actions } from './$types';
 import prisma from '$lib/server/prisma';
+import { errorHandler } from '$lib/server/errorHandler';
 
 export const actions: Actions = {
 	default: async (event) => {
@@ -22,44 +23,48 @@ export const actions: Actions = {
 			});
 		}
 
-		let isAdmin = false;
-		// TODO: better error handling
-		// try {
-		const existingUser = await prisma.user.findFirst({
-			where: {
-				id: id.toLowerCase()
-			}
-		});
-		if (!existingUser) {
+		const [existingUser, existingUserError] = await errorHandler(
+			prisma.user.findFirst({
+				where: {
+					id: id.toLowerCase()
+				}
+			})
+		);
+		if (!existingUser || existingUserError) {
 			return fail(400, {
 				message: 'Incorrect username or password'
 			});
 		}
 
-		const validPassword = await new Argon2id().verify(existingUser.password, password);
+		const [validPassword, verifyError] = await errorHandler(
+			new Argon2id().verify(existingUser.password, password)
+		);
 		if (!validPassword) {
 			return fail(400, {
 				message: 'Incorrect username or password'
 			});
 		}
+		if (verifyError) {
+			return fail(500, {
+				message: 'Internal server error'
+			});
+		}
 
-		const session = await lucia.createSession(existingUser.id, {});
-		const sessionCookie = lucia.createSessionCookie(session.id);
-		event.cookies.set(sessionCookie.name, sessionCookie.value, {
-			path: '.',
-			...sessionCookie.attributes
-		});
+		try {
+			const session = await lucia.createSession(existingUser.id, {});
+			const sessionCookie = lucia.createSessionCookie(session.id);
+			event.cookies.set(sessionCookie.name, sessionCookie.value, {
+				path: '.',
+				...sessionCookie.attributes
+			});
+		} catch (e) {
+			console.error(e);
+			return fail(500, {
+				message: 'Internal server error'
+			});
+		}
 
-		if (existingUser.role === 'admin') isAdmin = true;
-		isAdmin = false;
-		// } catch (e) {
-		// 	console.error(e);
-		// 	return fail(500, {
-		// 		message: 'Internal server error'
-		// 	});
-		// }
-
-		if (isAdmin) redirect(302, '/admin');
+		if (existingUser.role === 'admin') redirect(302, '/admin');
 		else redirect(302, '/');
 	}
 };

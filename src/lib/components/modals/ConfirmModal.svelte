93b873d8bc47301ab1de7ae6a377ea35fe1b83b0
El <evriel.leovaztion@gmail.com>
2024-05-03 14:49:13 +0700

    fixed userpanel modal ui

--- src/lib/components/modals/ConfirmModal.svelte
+++ src/lib/components/modals/ConfirmModal.svelte
@@ -10,20 +10,26 @@
 </script>
 
 {#if $modalStore[0]}
-	<div class="min-w-20">
+	<div class="min-w-20 p-4 bg-white rounded-md shadow-md shadow-[#979797]">
 		<form method="post" action="?/submitAction" use:enhance={onFormSubmit}>
-			<h3>Are you sure you want to submit for {selectedAction?.name ?? ''}?</h3>
-			<input class="input" type="hidden" name="actionId" value={selectedAction?.id} />
-			<input class="input" type="hidden" name="classId" value={selectedClass} />
-
-			<div class="flex items-center justify-center">
-				<button class="variant-outline btn" type="submit">Submit</button>
-				<button
-					class="variant-filled btn"
-					on:click={() => {
-						modalStore.close();
-					}}>Cancel</button
-				>
+			<div class="flex justify-end">
+				<button style="background: none; border: none; outline: none;" on:click={() => { modalStore.close(); }}>
+					<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+						<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="4" d="M6 18 17.94 6M18 18 6.06 6"/>
+					</svg>
+				</button>
+			</div>
+			<div class="items-center justify-center text-center px-8 pt-1 pb-4">
+				<h1 class="font-bold text-2xl">SUBMISSION CONFIRMATION</h1>
+				<div class="flex justify-center">
+					<h3 class="font-medium text-wrap w-4/5">Are you sure you want to submit for {selectedAction?.name ?? ''}?</h3>
+					<input class="input" type="hidden" name="actionId" value={selectedAction?.id} />
+				<input class="input" type="hidden" name="classId" value={selectedClass} />
+				</div>
+				
+				<div class="flex items-center justify-center pt-6">
+					<button class="btn bg-primary-400 py-3 px-10 text-white rounded-2xl font-bold hover:bg-primary-500 hover:drop-shadow-md text-xl" type="submit">SUBMIT</button>
+				</div>
 			</div>
 		</form>
 	</div>

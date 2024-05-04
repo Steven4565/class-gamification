6925bbfb6b29d387873cad91a41a4283e0fc195d
El <evriel.leovaztion@gmail.com>
2024-05-04 16:22:24 +0700

    added admintable
    not done, basic features

---
+++ src/lib/components/admin/RowsPerPage.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { DataHandler } from '@vincjo/datatables';
+	export let handler: DataHandler;
+	const rowsPerPage = handler.getRowsPerPage();
+	const options = [5, 10, 15];
+</script>
+
+<aside class="flex place-items-center pr-3">
+	Display
+	<select class="select ml-3 px-1 py-1 border-solid border-gray-400 text-sm rounded-md" bind:value={$rowsPerPage}>
+		{#each options as option}
+			<option value={option}>
+				{option}
+			</option>
+		{/each}
+	</select>
+</aside>

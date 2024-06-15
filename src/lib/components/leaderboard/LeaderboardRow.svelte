ddad6486081251a68138f8f2bc3d4eea6ffc2920
Steven Christian <stevenchristian4565@gmail.com>
2024-06-15 10:15:33 +0700

    leaderboard row ui

--- src/lib/components/leaderboard/LeaderboardRow.svelte
+++ src/lib/components/leaderboard/LeaderboardRow.svelte
@@ -4,17 +4,20 @@
 	export let rank: number;
 </script>
 
-<div class="items-centerp-3 mt-5 flex outline-4 outline-gray-300">
-	<div>
-		<h4>{rank}</h4>
-	</div>
-	<div class="mx-5 w-12 overflow-hidden rounded-full">
-		<!-- <img src="https://placekitten.com/100/100" alt="" /> -->
-		<!-- TODO: replace this  -->
-	</div>
-
-	<div class="grid grid-cols-2 items-center">
+<div class="mt-5 flex items-center justify-between rounded-xl border-2 border-gray-300 px-4 py-2">
+	<div class="flex gap-2">
+		<div>
+			<h4 class="font-bold">{rank}</h4>
+		</div>
+		<div class=" w-10 overflow-hidden rounded-full">
+			<img
+				src="https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg"
+				alt=""
+			/>
+		</div>
 		<h5>{name}</h5>
-		<p>{points} Points</p>
+	</div>
+	<div class="right">
+		<p class="p text-right font-bold text-primary-400">{points} Points</p>
 	</div>
 </div>

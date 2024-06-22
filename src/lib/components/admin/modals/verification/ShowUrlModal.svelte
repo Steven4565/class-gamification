3e5934639d6ac58c25c44f95e3366eec41ac5448
Steven Christian <stevenchristian4565@gmail.com>
2024-06-22 14:40:53 +0700

    removed invalidate button

--- src/lib/components/admin/modals/verification/ShowUrlModal.svelte
+++ src/lib/components/admin/modals/verification/ShowUrlModal.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
-	import { getModalStore, getToastStore, type ToastSettings } from '@skeletonlabs/skeleton';
-	import type { SubmitFunction } from '@sveltejs/kit';
-	import type { SvelteComponent } from 'svelte';
+	import { getModalStore } from '@skeletonlabs/skeleton';
 
-	const toastStore = getToastStore();
 	const modalStore = getModalStore();
 
 	const url = $modalStore[0]?.meta?.url;
@@ -13,9 +9,5 @@
 {#if $modalStore[0]}
 	<div class="w-full max-w-lg rounded-lg bg-lavenderMist p-10 shadow-inner drop-shadow-md">
 		<a href={url} target="_blank" class="anchor text-xl"> Open proof </a>
-
-		<form action="?/invalidate" method="post">
-			<button type="submit" class="btn bg-red-500 p-3 font-bold text-white"> Invalidate </button>
-		</form>
 	</div>
 {/if}
